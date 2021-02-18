2a5eed34b42631e062afcce619805fd63857fc37
Ivan <78233139+GentlemanGit@users.noreply.github.com>
2021-02-18 14:48:45 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let substr = []; // массив скобок которые нужно найти
+    // заполняем массив скобок подстроками
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        substr.push(String(bracketsConfig[i][0] + bracketsConfig[i][1]));
+    }
+    let newStr = str; //строка с которой будем работать
+    //Будем искать подстроку в строке пока не пройдём всю начальную строку
+    for (let i = 0; i < str.length; i++) {
+        // ищем подстроки в строке и заменяем их на пустое место
+        for (let i = 0; i < substr.length; i++) {
+            newStr = newStr.replace(substr[i], "");
+        }
+    }
+    // если строка пустая то скобки стояли правильно
+    if (newStr === "") return true;
+    else return false;
+};
+
